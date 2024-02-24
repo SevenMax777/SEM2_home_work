44410f5dea2cbfece598f3abf936b9d514a82c4b
Max <seven.85@list.ru>
2024-02-24 15:41:29 +0500

    problem_4 solved

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая на вход принимает натуральное
+// число N, а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int variable = number;
+
+while (variable != 0)
+{
+	variable = variable / 10;
+	i *= 10;
+}
+//Console.WriteLine(n);
+
+while (i > 1)
+{
+	i = i / 10;
+	variable = number / i;
+	if (i > 1)
+	{
+		Console.Write(variable + ", ");
+		number = number % i;
+	}
+	else
+	{
+		Console.Write(variable);
+	}
+}
